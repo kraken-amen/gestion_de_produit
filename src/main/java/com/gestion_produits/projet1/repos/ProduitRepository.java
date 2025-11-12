59a5710128324ba895592c2ccee51ff3979c16ee
Amen <krakenamen@gmail.com>
2025-11-12 13:45:32 +0100

    modifier

--- src/main/java/com/gestion_produits/projet1/repos/ProduitRepository.java
+++ src/main/java/com/gestion_produits/projet1/repos/ProduitRepository.java
@@ -2,6 +2,11 @@ package com.gestion_produits.projet1.repos;
 
 import com.gestion_produits.projet1.entities.Produit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProduitRepository extends JpaRepository<Produit,  Long> {
+    List<Produit> findByNomProduit(String nomProduit);
+
 }
